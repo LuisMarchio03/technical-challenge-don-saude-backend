a1caf5c2a9da249ab66b1707cebdad98750f18cb
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-10-18 10:40:24 -0300

    feat: add create associate controller

---
+++ src/module/http/controllers/create.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express'
+import z from 'zod'
+
+import { makeCreateAssociateUseCase } from '../../use-cases/factories/make-create-associate-use-case' 
+
+export class CreateController {
+
+  async handler(req: Request, res: Response) {
+    try {
+      const createBodySchema = z.object({
+        name: z.string(),
+        email: z.string(),
+        cnpj: z.string(),
+        phone: z.string(),
+        cellphone: z.string(),
+        responsible_clinic: z.string(),
+        responsible_finance: z.string(),
+        password: z.string(),
+      })
+    
+      const { 
+        name,
+        email,
+        cnpj,
+        phone,
+        cellphone,
+        responsible_clinic,
+        responsible_finance,
+        password,
+       } = createBodySchema.parse(req.body)
+    
+      const createGymUseCase = makeCreateAssociateUseCase()
+    
+      await createGymUseCase.execute({
+        name,
+        email,
+        cnpj,
+        phone,
+        cellphone,
+        responsible_clinic,
+        responsible_finance,
+        password,
+      })
+    
+      return res.status(201).send()
+    } catch (err: any) {
+      return res.status(400).json({ error: err.message })
+    }
+  }
+
+}
