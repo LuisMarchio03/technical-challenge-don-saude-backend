86e3e2e42cde3f0daed9a780c6dec1c083de97ad
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-10-18 12:51:27 -0300

    feat: add list associates in routes

--- src/module/http/controllers/routes/index.ts
+++ src/module/http/controllers/routes/index.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { CreateController } from "../create"
+import { ListAssociatesController } from "../list-associates"
+import { ListAssociateController } from "../list-associate";
 
 const associateRoutes = Router();
 
 const createController = new CreateController();
+const listAssociatesController = new ListAssociatesController();
+const listAssociateController = new ListAssociateController();
 
 associateRoutes.post("/associate/create", createController.handler);
+associateRoutes.get("/list/associates", listAssociatesController.handler);
+associateRoutes.get("/list/associate/:id", listAssociateController.handler);
 
 export { associateRoutes };
