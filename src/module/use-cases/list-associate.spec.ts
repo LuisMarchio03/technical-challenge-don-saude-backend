f169fb2fe40e4a06e6ca2da294bf4c597c9b2403
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-10-18 12:51:12 -0300

    test: add find one associate use case

---
+++ src/module/use-cases/list-associate.spec.ts
@@ -0,0 +1,24 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+
+import { ListAssociateUseCase } from './list-associate'
+import { InMemoryAssociatesRepository } from '../repositories/in-memory/in-memory-associates-repository'
+import { ListAssociatesUseCase } from './list-associates'
+    
+let sut: ListAssociateUseCase
+let listAssociatesUseCase: ListAssociatesUseCase
+let associatesRepository: InMemoryAssociatesRepository
+
+describe('List Associate Use Case', () => {
+  beforeEach(() => {
+    associatesRepository = new InMemoryAssociatesRepository()
+    sut = new ListAssociateUseCase(associatesRepository)
+    listAssociatesUseCase = new ListAssociatesUseCase(associatesRepository)
+  })
+
+  it('should be able to List one Associate', async () => {
+    const associates = await listAssociatesUseCase.execute()
+    associates.find(async associate => {
+      expect(await sut.execute(associate.id)).toEqual(expect.any(Object))
+    })
+  })
+})
