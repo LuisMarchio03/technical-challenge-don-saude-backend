804abd971d59c453c23332c76bd307fc93d6d35c
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-10-18 12:49:15 -0300

    test: add find all associates use case

---
+++ src/module/use-cases/list-associates.spec.ts
@@ -0,0 +1,18 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+
+import { ListAssociatesUseCase } from './list-associates'
+import { InMemoryAssociatesRepository } from '../repositories/in-memory/in-memory-associates-repository'
+    
+let sut: ListAssociatesUseCase
+let associatesRepository: InMemoryAssociatesRepository
+
+describe('List Associate Use Case', () => {
+  beforeEach(() => {
+    associatesRepository = new InMemoryAssociatesRepository()
+    sut = new ListAssociatesUseCase(associatesRepository)
+  })
+
+  it('should be able to List Associate', async () => {
+    expect(await sut.execute()).toEqual(expect.any(Array))
+  })
+})
