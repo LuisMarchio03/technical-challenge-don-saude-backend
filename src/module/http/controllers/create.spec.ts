c857e2436b97a6ed0ab97e2dd3d340b3944a7a53
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-10-18 10:41:08 -0300

    test: add create associate controller

---
+++ src/module/http/controllers/create.spec.ts
@@ -0,0 +1,40 @@
+import { prisma } from '../../lib/prisma'
+import { app } from '../../../app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+
+describe('Create Associate (e2e)', () => {
+  afterAll(async () => {
+    const existingAssociate = await prisma.associate.findUnique({
+      where: {
+        email: "any@gmail.com",
+      },
+    });
+
+    if (existingAssociate) {
+      await prisma.associate.delete({
+        where: {
+          email: "any@gmail.com",
+        },
+      });
+    }
+  });
+
+  it('should be able to create a associate', async () => {
+    const profileResponse = await request(app)
+      .post('/associate/create')
+      .send({
+        name: "any",
+        email: "any@gmail.com",
+        cnpj: "123123123",
+        phone: "(64) 9 99991-8525",
+        cellphone: "(64) 9 99991-8525",
+        responsible_clinic: "any 01",
+        responsible_finance: "any 02",
+        password: "123456"
+      })
+
+    expect(profileResponse.statusCode).toEqual(201)
+  })
+})
