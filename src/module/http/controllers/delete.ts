f6c8f6af7e054b8631bddaa2518829c143bfe0c6
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-10-18 13:30:32 -0300

    feat: add delete associate controller

---
+++ src/module/http/controllers/delete.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+
+import { makeDeleteAssociateUseCase } from '../../use-cases/factories/make-delete-associate-use-case'
+
+export class ListAssociateController {
+
+  async handler(req: Request, res: Response) {
+    try {
+      const { id } = req.params
+
+      const deleteAssociateUseCase = makeDeleteAssociateUseCase()
+      const associates = await deleteAssociateUseCase.execute(id)
+    
+      return res.status(200).json(associates)
+    } catch (err: any) {
+      return res.status(400).json({ error: err.message })
+    }
+  }
+
+}
