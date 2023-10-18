f6e774ebb4cdb60e3afa66e70d912cf9645a7501
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-10-18 12:50:10 -0300

    feat: add find one associate use case

---
+++ src/module/http/controllers/list-associate.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+
+import { makeListAssociateUseCase } from '../../use-cases/factories/make-list-associate-use-case' 
+
+export class ListAssociateController {
+
+  async handler(req: Request, res: Response) {
+    try {
+      const { id } = req.params
+
+      const listAssociateUseCase = makeListAssociateUseCase()
+      const associates = await listAssociateUseCase.execute(id)
+    
+      return res.status(200).json(associates)
+    } catch (err: any) {
+      return res.status(400).json({ error: err.message })
+    }
+  }
+
+}
