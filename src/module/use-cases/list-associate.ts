619be8e3d6ffd9ce2a5e806f03848e8cf599fed3
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-10-18 12:50:44 -0300

    feat: add find one associate use case

---
+++ src/module/use-cases/list-associate.ts
@@ -0,0 +1,10 @@
+import { Associate } from '@prisma/client'
+import { AssociatesRepository } from '../repositories/associates-repository'
+
+export class ListAssociateUseCase {
+  constructor(private AssociatesRepository: AssociatesRepository) {}
+  
+  async execute(id: string): Promise<Associate> {
+    return await this.AssociatesRepository.listAssociate(id)
+  }
+}
