c62634e446aad7268f4f1ba409080c62c389ed9a
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-10-18 10:39:26 -0300

    feat: add create associate use-case

---
+++ src/module/use-cases/create-associate.ts
@@ -0,0 +1,56 @@
+import { AssociatesRepository } from '../repositories/associates-repository'
+
+interface CreateAssociateUseCaseRequest {
+  name: string
+  email: string
+  cnpj: string
+  phone: string
+  cellphone: string
+  responsible_clinic: string
+  responsible_finance: string
+  password: string
+}
+
+interface CreateAssociateUseCaseResponse {
+  name: string
+  email: string
+  cnpj: string
+  phone: string
+  cellphone: string
+  responsible_clinic: string
+  responsible_finance: string
+  password: string
+}
+
+export class CreateAssociateUseCase {
+  constructor(private AssociatesRepository: AssociatesRepository) {}
+  
+  async execute({
+    name,
+    email,
+    cnpj,
+    phone,
+    cellphone,
+    responsible_clinic,
+    responsible_finance,
+    password,
+  }: CreateAssociateUseCaseRequest): Promise<CreateAssociateUseCaseResponse> {
+    const associatesAlreadyExists = await this.AssociatesRepository.findByEmail(email)
+
+    if (associatesAlreadyExists) {
+      throw new Error('Associate already exists')
+    }
+
+    const associate = await this.AssociatesRepository.create({
+      name,
+      email,
+      cnpj,
+      phone,
+      cellphone,
+      responsible_clinic,
+      responsible_finance,
+      password,
+    })
+    return associate
+  }
+}
