cc297e5611c414c9a50387ea20d4a5385ab686a4
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-10-18 14:40:27 -0300

    feat: add delete address controller

---
+++ src/module/http/controllers/delete-address.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+
+import { makeDeleteAssociateAddressUseCase } from '../../use-cases/factories/make-delete-associate-address-use-case'
+
+export class DeleteAssociateAddressController {
+
+  async handler(req: Request, res: Response) {
+    try {
+      const { id } = req.params
+
+      const deleteAssociateAddressUseCase = makeDeleteAssociateAddressUseCase()
+      const associates = await deleteAssociateAddressUseCase.execute(id)
+    
+      return res.status(200).json(associates)
+    } catch (err: any) {
+      return res.status(400).json({ error: err.message })
+    }
+  }
+
+}
